92c3b7c9b4693f21552ead8d9010bf0300ede08b
Gabriel Lebec <glebec@gmail.com>
2019-06-04 19:42:10 -0400

    chore(form): fix formatting

--- src/Form.js
+++ src/Form.js
@@ -1,26 +1,49 @@
 import React from 'react'
 
 const Form = props => {
+  const {
+    taskName,
+    assignee,
+    handleChange,
+    handleSubmit,
+    warningMessage,
+    errorMessage,
+  } = props
 
-  const {taskName, assignee, handleChange, handleSubmit, warningMessage, errorMessage } = props
+  return (
+    <form id="todo-form" onSubmit={handleSubmit}>
+      <label htmlFor="taskName">
+        Task Name:
+        {!taskName && warningMessage && (
+          <span className="warning">{warningMessage}</span>
+        )}
+      </label>
+      <input
+        name="taskName"
+        type="text"
+        onChange={handleChange}
+        value={taskName}
+      />
 
-return  <form id='todo-form' onSubmit={handleSubmit}>
+      <label htmlFor="assignee">
+        Assign To:
+        {!assignee && warningMessage && (
+          <span className="warning">{warningMessage}</span>
+        )}
+      </label>
+      <input
+        name="assignee"
+        type="text"
+        onChange={handleChange}
+        value={assignee}
+      />
 
-    <label htmlFor='taskName'>
-      Task Name:
-      {!taskName && warningMessage && <span className='warning'>{warningMessage}</span>}
-    </label>
-    <input name='taskName' type='text' onChange={handleChange} value={taskName} />
-
-    <label htmlFor='assignee'>
-      Assign To:
-      {!assignee && warningMessage && <span className='warning'>{warningMessage}</span>}
-    </label>
-    <input name='assignee' type='text' onChange={handleChange} value={assignee} />
-
-    <button type='submit' disabled={!taskName || !assignee}>Submit</button>
-    {errorMessage && <div className='error'>{errorMessage}</div>}
-  </form>
+      <button type="submit" disabled={!taskName || !assignee}>
+        Submit
+      </button>
+      {errorMessage && <div className="error">{errorMessage}</div>}
+    </form>
+  )
 }
 
 export default Form
